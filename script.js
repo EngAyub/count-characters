bcde8f12ebff5f7d26f7976eb26bb8e6fa92de00
ENG AYUUB <moztaphem9@gmal.com>
2023-06-17 19:46:15 +0300

    Count letters In string done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function count(num, string) 
+{
+    let count = 0
+    for (let i = 0; i < string.length; i++) 
+    {
+      if (string[i] == num) 
+      {
+        count++;
+      }
+    }
+    return count;
+  }
+let leter = "c"
+let sentence = "fikrcamp coding Bootcamps"
+let Result = count(leter, sentence)
+    console.log(Result);
